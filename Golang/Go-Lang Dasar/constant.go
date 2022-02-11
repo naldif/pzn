101910a439fdfe3d608af35525287bce2af1f9a3
naldif <nitoaldif@gmail.com>
2022-02-11 10:53:50 +0700

    perbaikan folder golang

---
+++ Golang/Go-Lang Dasar/constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Eko"
+	const lastName = "Aldif"
+	const value = 100
+
+	// fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
---
+++ Golang/Go-Lang Dasar/variable.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Nito Aldi"
+	fmt.Println(name);
+
+	name = "Nito Ganteng"
+	fmt.Println(name);
+
+	var friendName = "Husen"
+	fmt.Println(friendName);
+
+	var age = "25"
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Nito"
+		lastName = "Aldif"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
