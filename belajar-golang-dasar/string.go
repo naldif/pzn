e60ca7c86119f1b05df1fd5e96503d585b1a21e7
naldif <nitoaldif@gmail.com>
2022-02-11 10:23:12 +0700

    golang : string & variable

--- belajar-golang-dasar/string.go
+++ belajar-golang-dasar/string.go
@@ -5,4 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Nito")
 	fmt.Println("Nito Aldi")
+	fmt.Println("Nito Aldi Fitrian")
+
+	fmt.Println(len("Nito"))
+	fmt.Println("Nito Aldi"[0])
+	fmt.Println("Nito Aldi Fitrian"[1])
 }
---
+++ belajar-golang-dasar/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Nito Aldi"
+	fmt.Println(name);
+
+	name = "Nito Ganteng"
+	fmt.Println(name);
+}
