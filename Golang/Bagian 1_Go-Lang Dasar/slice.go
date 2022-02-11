3dc2b8c353b5c035d975db3243cb29455d96f9e6
naldif <nitoaldif@gmail.com>
2022-02-11 16:36:32 +0700

    arrat, boolean-operator, slice

---
+++ Golang/Bagian 1_Go-Lang Dasar/slice.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+
+	var slice1 = months[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	// months[5] = "Diubah"
+	// fmt.Println(slice1)
+
+	slice1[0] = "Mei Lagi"
+	fmt.Println(months)
+
+	var slice2 = months[10:]
+	fmt.Println(slice2)
+
+	var slice3 = append(slice2, "Nito")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan Desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(months)
+
+}
